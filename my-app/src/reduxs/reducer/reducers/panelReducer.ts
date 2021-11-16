cc95e5f96d49a6f87741e85db7614cddde928724
[refrrir] <[liu.qihu@northeatern.edu]>
2021-11-16 15:14:34 -0800

    add local storage

--- my-app/src/reduxs/reducer/reducers/panelReducer.ts
+++ my-app/src/reduxs/reducer/reducers/panelReducer.ts
@@ -6,10 +6,18 @@ const defaultState: IPanelState = {
     initial: true,
 };
 
-export default (state: IPanelState = defaultState, action: Action): IPanelState => {
+export default (state: IPanelState, action: Action): IPanelState => {
+    if (state == null){
+        if (localStorage.getItem("panelState") !== null){
+            state = JSON.parse(localStorage.getItem("panelState")!);
+        }else{
+            state = defaultState;
+        }
+    }
     const { key, value } = action;
     if (action.type === constant.UPDATE_PANEL_STATE) {
         Object.defineProperty(state, key, { value: value });
+        localStorage.setItem("panelState", JSON.stringify({ ...state }));
         return { ...state };
     }
     else {
